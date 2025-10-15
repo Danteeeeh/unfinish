a9f5a77548070224dccfaba97b3f37e03b7661a8
Danteeeeh <119085318+Danteeeeh@users.noreply.github.com>
2025-10-15 11:02:31 +0800

    Delete dnms-php directory

--- dnms-php/controllers/auth/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('ROOT_PATH', dirname(dirname(__DIR__)));
-require_once ROOT_PATH . '/config/environment.php';
-
-logout();
-setFlash('success', 'You have been logged out successfully');
-redirect(APP_URL . '/controllers/auth/login.php');
